e5c9391beb09462f71b676322e53a21d9fb579f4
ishmeet5ingh <ishmeet3j@gmail.com>
2024-08-26 10:29:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+   <>
+  <h1 className='text-3xl text-center  text-black'>Resourse Management Admin Portal</h1>
+   </>
+  )
+}
+
+export default App
